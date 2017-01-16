07318a7fcd5693b8f6866f567cbc6b7c579235bc
Malcolm B Anderson <malcolm.b.anderson@pragmaticagility.com>
2017-01-16 05:54:16 +0000

    cleaning things up

--- app.js
+++ app.js
@@ -1,86 +1,62 @@
-var express               = require("express"),
-    mongoose              = require("mongoose"),
-    passport              = require("passport"),
-    bodyParser            = require("body-parser"),
-    User                  = require("./models/user"),
-    LocalStrategy         = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose")
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    User                    = require ("./models/user"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+//    ejs                     = require("ejs"),
+//    expressSession          = require("express-session");
     
+
+
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
+
+
+// APPLICATION CONFIGURATION
 var app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
 app.use(require("express-session")({
-    secret: "Rusty is the best and cutest dog in the world",
+    secret: "Rusty is best, cutest dog in the world",
     resave: false,
     saveUninitialized: false
 }));
 
+//console.log(passport);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//============
+
+// ==============
 // ROUTES
-//============
+// ==============
 
 app.get("/", function(req, res){
-    res.render("home");
+   res.render("home"); 
 });
 
-app.get("/secret",isLoggedIn, function(req, res){
-   res.render("secret"); 
-});
-
-// Auth Routes
-
-//show sign up form
-app.get("/register", function(req, res){
-   res.render("register"); 
-});
-//handling user sign up
-app.post("/register", function(req, res){
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            return res.render('register');
-        }
-        passport.authenticate("local")(req, res, function(){
-           res.redirect("/secret");
-        });
-    });
-});
-
-// LOGIN ROUTES
-//render login form
-app.get("/login", function(req, res){
-   res.render("login"); 
-});
-//login logic
-//middleware
-app.post("/login", passport.authenticate("local", {
-    successRedirect: "/secret",
-    failureRedirect: "/login"
-}) ,function(req, res){
-});
-
-app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
-});
+app.get("/secret", function(req, res){
+    res.render("secret");
+})
 
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+
+
+
+
+
+
+// ==============
+// LISTENER
+// ==============
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("server started.......");
-})
+   console.log("server started ......") 
+});
