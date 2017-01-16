49971ce23da327463a7a7439ae212cead86d6ddf
Malcolm B Anderson <malcolm.b.anderson@pragmaticagility.com>
2017-01-16 07:42:22 +0000

    Complete Part 4, ready for part 5

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var express                 = require("express"),
 
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
+mongoose.Promise = global.Promise;
 
 
 // APPLICATION CONFIGURATION
@@ -30,6 +31,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -76,6 +78,22 @@ app.post("/register", function(req, res){
     });
 });
 
+
+
+// ==============
+// LOGIN ROUTES
+// ==============
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+// this is our login route and it's ugly.
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
+});
 
 
 // ==============
