4a2aab8fda03a0b4a4d48d190a2e7a5f6abb90ef
Malcolm B Anderson <malcolm.b.anderson@pragmaticagility.com>
2017-01-16 07:21:09 +0000

    Completed 32-269 (Auth CodeAlong Part 3)

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 // APPLICATION CONFIGURATION
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(require("express-session")({
     secret: "Rusty is the best and cutest dog in the world",
@@ -46,9 +47,34 @@ app.get("/secret", function(req, res){
 })
 
 
+// ==============
+// AUTH ROUTES
+// ==============
 
+// show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
 
-
+// handling user sign up
+app.post("/register", function(req, res){
+    //res.send("REGISTER POST ROUTE");
+    req.body.username;
+    req.body.password;
+    User.register(
+            new User({username: req.body.username}), 
+            req.body.password, 
+            function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/secret"); 
+        });
+        
+    });
+});
 
 
 
